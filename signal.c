4fcd8890956074f397d22cdf810b460a13ff70ea
Ayhan Dalkıran <69454747+AyhanDalkiran@users.noreply.github.com>
2026-05-24 02:29:19 +0300

    added counter

--- signal.c
+++ signal.c
@@ -7,30 +7,42 @@
 #include <stdbool.h>
 
 // Child Signal Handler
-void sig_int() {
+void sig_int(int signum) {
+    (void)signum; // Unused parameter, compiler does not shut up about it
     fprintf(stdout, "Child: SIGINT received but continuing...\n");
 
     // Override SIG_DFL behaviour
     signal(SIGINT, sig_int);
 }
 
-void sig_cont() {
+void sig_cont(int signum) {
+    (void)signum; // Unused parameter, compiler does not shut up about it
     fprintf(stdout, "Child: Process resumed\n");
 
     // Override SIG_DFL behaviour
     signal(SIGCONT, sig_cont);
 }
 
+void sig_alrm_child(int signum) {
+    (void)signum; // Unused parameter, compiler does not shut up about it
+    static unsigned int child_timer = 0;
+    fprintf(stdout, "Child counter: %u\n", child_timer);
+    child_timer++;
+    alarm(1);
+
+    // Override SIG_DFL behaviour
+    signal(SIGALRM, sig_alrm_child);
+}
+
 pid_t child_pid;
-unsigned int timer = 0;
+unsigned int parent_timer = 0;
 bool child_running = true;
 
 // Parent Signal Handler
-void sig_alrm() {
-    alarm(0); // Cancel any pending alarms
-
+void sig_alrm_parent(int signum) {
+    (void)signum; // Unused parameter, compiler does not shut up about it
     if (child_running) {
-        timer += 3;
+        parent_timer += 3;
 
         fprintf(stdout, "Parent: Stopping child...\n");
         kill(child_pid, SIGSTOP);
@@ -39,7 +51,7 @@ void sig_alrm() {
         alarm(2);
     }
     else {
-        timer += 2;
+        parent_timer += 2;
 
         fprintf(stdout, "Parent: Continuing child...\n");
         kill(child_pid, SIGCONT);
@@ -48,12 +60,12 @@ void sig_alrm() {
         alarm(3);
     }
 
-    signal(SIGALRM, sig_alrm);
+    signal(SIGALRM, sig_alrm_parent);
 }
 
 int main(void) {
     if ((child_pid = fork()) == -1) { // Fork failure
-        fprintf(stderr, "error: Could not fork process");
+        fprintf(stderr, "error: Could not fork process\n");
         return EXIT_FAILURE;
     }
 
@@ -63,20 +75,22 @@ int main(void) {
     if (child_pid == 0) { // Child Process
         signal(SIGINT, sig_int);
         signal(SIGCONT, sig_cont);
+        signal(SIGALRM, sig_alrm_child);
+        alarm(1);
 
         while(true);
     }
     else { // Parent Process
-        signal(SIGALRM, sig_alrm);
+        signal(SIGALRM, sig_alrm_parent);
 
         alarm(3);
-        while(timer <= 10 || !child_running);
+        while(parent_timer < 10 || !child_running);
 
         fprintf(stdout, "Parent: Sending SIGINT...\n");
         kill(child_pid, SIGINT);
 
-        fprintf(stdout, "Parent: Sending SIGTERM...\n");
         kill(child_pid, SIGTERM);
+        fprintf(stdout, "Parent: Child terminated.\n");
     }
 
     return EXIT_SUCCESS;
